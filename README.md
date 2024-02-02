a68478bae253de3c9ac19de4908fd2b1a19e2856
Ameereeddeen11 <89810178+Ameereeddeen11@users.noreply.github.com>
2024-02-02 10:24:53 +0100

    path parameters

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# FastAPI-mini-projects
+# FastAPI mini projects
+
+This repository is dedicated to showcasing my learning journey FastAPI by creating various mini projects. Through this 
+repository, I aim to share my progress and the different applications I've built while learning FastAPI.
--- main.py
+++ main.py
@@ -1,9 +1,42 @@
 from fastapi import FastAPI
+from enum import Enum
 
 app = FastAPI()
 
+class DayName(int, Enum):
+    Mondey = 1
+    Tuesday = 2
+    Wednesday = 3
+    Thorusday = 4
+    Friday = 5
+    Saturday = 6
+    Sunday = 7
 
 @app.get("/")
 async def home():
     return {"message": "Hello World"}
 
+# Path parameters
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}
+
+@app.get("/today/{day}")
+async def day(day: DayName):
+    match day:
+        case DayName.Mondey:
+            return {"day": "Mondey"}
+        case DayName.Tuesday:
+            return {"day": "Tuesday"}
+        case DayName.Wednesday:
+            return {"day": "Wednesday"}
+        case DayName.Thorusday:
+            return {"day": "Thorusday"}
+        case DayName.Friday:
+            return {"day": "Friday"}
+        case DayName.Saturday:
+            return {"day": "Saturday"}
+        case DayName.Sunday:
+            return {"day": "Sunday"}
+        case _:
+            return {"day": "Invalid day"}
