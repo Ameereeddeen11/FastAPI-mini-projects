17ff1e5d88ca7d0a656e60d71afbd883366b60ef
Ameereeddeen11 <89810178+Ameereeddeen11@users.noreply.github.com>
2024-02-02 13:54:04 +0100

    learning query parameter

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ async def read_item(item_id: int):
 async def day(day: DayName):
     match day:
         case DayName.Mondey:
-            return {"day": "Mondey"}
+            return {"day": "Monday"}
         case DayName.Tuesday:
             return {"day": "Tuesday"}
         case DayName.Wednesday:
@@ -40,3 +40,36 @@ async def day(day: DayName):
             return {"day": "Sunday"}
         case _:
             return {"day": "Invalid day"}
+
+# Query parameters
+@app.get("/query/")
+async def query(item_id: int, day: DayName | None = None):
+    if day:
+        match day:
+            case DayName.Mondey:
+                return {"day": "Monday", "item_id": item_id}
+            case DayName.Tuesday:
+                return {"day": "Tuesday", "item_id": item_id}
+            case DayName.Wednesday:
+                return {"day": "Wednesday", "item_id": item_id}
+            case DayName.Thorusday:
+                return {"day": "Thorusday", "item_id": item_id}
+            case DayName.Friday:
+                return {"day": "Friday", "item_id": item_id}
+            case DayName.Saturday:
+                return {"day": "Saturday", "item_id": item_id}
+            case DayName.Sunday:
+                return {"day": "Sunday", "item_id": item_id}
+            case _:
+                return {"day": "Invalid day", "item_id": item_id}
+    else:
+        return {"item_id": item_id}
+
+@app.get("/query2/{item_id}/{user}/")
+async def query2(item_id: int, user: str, item: str = None, sold: bool = False):
+    result = {"item_id": item_id, "user": user}
+    if item:
+        result.update({"item": item})
+    if sold:
+        result.update({"sold": sold})
+    return result
