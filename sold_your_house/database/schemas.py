73f42ba3c707183bfee3cc77af23db95443377cb
Ameereeddeen11 <89810178+Ameereeddeen11@users.noreply.github.com>
2024-04-11 12:50:50 +0200

    edit schemas of database

--- sold_your_house/database/schemas.py
+++ sold_your_house/database/schemas.py
@@ -4,7 +4,7 @@ from typing import Optional
 class BaseHouse(BaseModel):
     address: str
     price: int
-    owner: int
+    #owner: int
 
 class House(BaseHouse):
     id: int
@@ -20,14 +20,3 @@ class User(BaseUser):
     id: int
     class Config:
         orm_mode = True
-
-
-'''class BaseUser(BaseModel):
-    username: str
-    email: str
-    house: Optional[int]
-
-class House(BaseModel):
-    address: str
-    price: int
-    owner: Optional[int]'''
