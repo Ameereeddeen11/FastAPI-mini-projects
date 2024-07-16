ebc9a55475f1c829cf2319dc29960df6ccbb690b
Ameereeddeen11 <89810178+Ameereeddeen11@users.noreply.github.com>
2024-07-16 17:06:49 +0200

    edit docker-compose and start auth

--- note-app/db/db.py
+++ note-app/db/db.py
@@ -4,8 +4,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-DATABASE_URL = "postgresql://postgres:bUeN0@db:5432/postgres"
+load_dotenv()
 
-engine = create_engine(DATABASE_URL)
+DB_URL = os.getenv("DB_URL")
+
+engine = create_engine(DB_URL)
 SessionLocal = sessionmaker(autocomplete=True, bind=engine)
 Base = declarative_base()
