83506cb3efc47cf295b2382cb97fd2264c18c60c
Ameereeddeen11 <89810178+Ameereeddeen11@users.noreply.github.com>
2024-07-16 16:56:01 +0200

    setup migration and dockerizetion

--- note-app/db/db.py
+++ note-app/db/db.py
@@ -1,8 +1,10 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-DATABASE_URL = "sqlite:///./note-app.db"
+DATABASE_URL = "postgresql://postgres:bUeN0@db:5432/postgres"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocomplete=True, bind=engine)
--- note-app/main.py
+++ note-app/main.py
@@ -1,6 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from db.db import SessionLocal, engine
+from sqlalchemy.orm import Session
+from typing import Annotated
+from db.models import *
 
 app = FastAPI()
+
+Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+db_dependency = Annotated[Session, Depends(get_db)]
 
 @app.get("/")
 async def home():
